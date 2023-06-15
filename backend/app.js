84d2846afc52bf6388c6e739475b3905b451a5f8
renan-feitosa <renan.oliveira@sou.inteli.edu.br>
2023-06-15 09:59:41 -0300

    v01

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const sqlite3 = require('sqlite3').verbose();
+const PATH = "./data/database.db";
+
+app.listen(3000)
+app.use(express.static("./frontend/"));
+app.use(express.json())
+
+app.post('/enviar_mensagem', (req, res) => {
+    var db = new sqlite3.Database(PATH);
+	sql = "INSERT INTO Tbl_Mensagem (MENSAGEM) VALUES ('" + req.body.mensagem + "')";
+	console.log(sql);
+	db.run(sql, [], err => {
+		if (err) {
+			throw err;
+		}
+	});
+	res.json({ mensagem: "Mensagem enviada!" }); // Retornar uma resposta JSON
+	db.close();
+	res.end();
+	res.redirect('/')
+})
+
+app.get('/listar_mensagem', (req, res) => {
+    var db = new sqlite3.Database(PATH);
+    sql = "SELECT MENSAGEM FROM Tbl_Mensagem"
+    db.all(sql, [], (err, rows) => {
+		if (err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+	db.close();
+})
